d4fd73b71a56ddf00201204a931e33705c86252e
lyw2015 <1319404727@qq.com>
2022-04-24 17:05:51 +0800

    sdc

---
+++ sentinel-dashboard/startup.cmd
@@ -0,0 +1,10 @@
+java ^
+-Dserver.port=8858 ^
+-Dcsp.sentinel.dashboard.server=localhost:8858 ^
+-Dproject.name=sentinel-dashboard ^
+-Dsentinel.dashboard.auth.username=sentinel ^
+-Dsentinel.dashboard.auth.password=sentinel ^
+-Dsentinel.dashboard.nacos.serverAddr=127.0.0.1:8848 ^
+-Dsentinel.dashboard.nacos.namespace=18808baf-2238-4f27-be8b-6cfa3680a0a2 ^
+-Dsentinel.dashboard.nacos.groupId=SENTINEL_GROUP ^
+-jar sentinel-dashboard.jar
