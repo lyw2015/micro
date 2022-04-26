87f3ceca40a5217531af7534737a039483b81892
lyw2015 <1319404727@qq.com>
2022-04-26 14:20:59 +0800

    fix sentinel

---
+++ micro-core/core-sentinel/src/main/java/com/laiyw/micro/sentinel/SentinelConfiguration.java
@@ -0,0 +1,16 @@
+package com.laiyw.micro.sentinel;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ProjectName micro
+ * @Author Laiyw
+ * @CreateTime 2022/4/26 14:15
+ * @Description TODO
+ */
+
+@ComponentScan(basePackages = "com.laiyw.micro.sentinel")
+@Configuration
+public class SentinelConfiguration {
+}
--- micro-services/ms-order/order-service/src/main/java/com/laiyw/micro/order/service/OrderServiceApplication.java
+++ micro-services/ms-order/order-service/src/main/java/com/laiyw/micro/order/service/OrderServiceApplication.java
@@ -2,13 +2,14 @@ package com.laiyw.micro.order.service;
 
 import com.laiyw.micro.feign.FeignConfiguration;
 import com.laiyw.micro.mybatis.MybatisPlusConfig;
+import com.laiyw.micro.sentinel.SentinelConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({MybatisPlusConfig.class, FeignConfiguration.class})
+@Import({MybatisPlusConfig.class, FeignConfiguration.class, SentinelConfiguration.class})
 @EnableFeignClients(basePackages = {"com.laiyw.micro.portal.api.client"})
 public class OrderServiceApplication {
 
