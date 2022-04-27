5f6f7b4ad8472288b064327e1dfba587043e58ac
lyw2015 <1319404727@qq.com>
2022-04-27 17:50:26 +0800

    aspect

--- micro-core/core-seata/src/main/java/com/laiyw/micro/seata/aspect/GlobalTransactionalAspect.java
+++ micro-core/core-seata/src/main/java/com/laiyw/micro/seata/aspect/GlobalTransactionalAspect.java
@@ -5,9 +5,12 @@ import io.seata.core.context.RootContext;
 import io.seata.core.exception.TransactionException;
 import io.seata.tm.api.GlobalTransactionContext;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 /**
  * @ProjectName micro
@@ -20,12 +23,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class GlobalTransactionalAspect {
 
-    @AfterThrowing(throwing = "e", pointcut = "execution(* com.laiyw.micro..service..*.*(..))")
-    public void doRecoveryActions(Throwable e) throws TransactionException {
-        log.error("业务方法执行异常: {}", e.getMessage());
+    @AfterThrowing(throwing = "e", pointcut = "execution(* com.laiyw.micro..service.impl..*.*(..))")
+    public void doRecoveryActions(JoinPoint joinPoint, Throwable e) throws TransactionException {
+        log.error("异常信息: {}", e.getMessage());
+        log.error("业务方法: {}", joinPoint.getSignature().toLongString());
+        log.error("业务参数： {}", Arrays.toString(joinPoint.getArgs()));
         String xid = RootContext.getXID();
         if (StringUtils.isNotBlank(xid)) {
-            log.info("回滚全局事务ID: {}", xid);
+            log.error("回滚全局事务ID: {}", xid);
             GlobalTransactionContext.reload(xid).rollback();
         }
     }
