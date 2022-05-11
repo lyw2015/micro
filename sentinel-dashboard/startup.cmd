6a030ac038a439a97134e466feeb088c27e598e1
lyw2015 <1319404727@qq.com>
2022-05-11 16:39:08 +0800

    adress

--- sentinel-dashboard/startup.cmd
+++ sentinel-dashboard/startup.cmd
@@ -1,10 +1,11 @@
 java ^
 -Dserver.port=8858 ^
--Dcsp.sentinel.dashboard.server=localhost:8858 ^
 -Dproject.name=sentinel-dashboard ^
 -Dsentinel.dashboard.auth.username=sentinel ^
 -Dsentinel.dashboard.auth.password=sentinel ^
--Dsentinel.dashboard.nacos.serverAddr=127.0.0.1:8844 ^
+-Dsentinel.dashboard.nacos.serverAddr=192.168.47.112:8848 ^
 -Dsentinel.dashboard.nacos.namespace=18808baf-2238-4f27-be8b-6cfa3680a0a2 ^
 -Dsentinel.dashboard.nacos.groupId=SENTINEL_GROUP ^
 -jar sentinel-dashboard.jar
+
+rem -Dcsp.sentinel.dashboard.server=localhost:8858 ^
