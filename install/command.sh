543a00dfe43d8122efacd136a98495fffc520540
Laiyw <1319404727@qq.com>
2022-05-15 17:39:18 +0800

    keepalived

--- install/command.sh
+++ install/command.sh
@@ -21,7 +21,8 @@ vim /etc/docker/daemon.json
 #}
 
 # 下载docker-compose
-wget https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -O /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+wget https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -O /usr/local/bin/docker-compose &&
+  chmod +x /usr/local/bin/docker-compose
 
 # 禁止防火墙开机自启
 systemctl stop firewalld && systemctl disable firewalld
@@ -53,3 +54,26 @@ docker-compose -f /data/docker-compose/service.yaml up -d --no-deps gateway
 
 # 查看服务日志
 docker-compose -f /data/docker-compose/service.yaml logs id-service
+
+# 安装keepalived
+# 基础依赖
+yum install -y gcc openssl-devel libnl libnl-devel libnfnetlink-devel net-tools
+# 下载、解压、改名、删除源文件、进入目录
+wget https://www.keepalived.org/software/keepalived-2.2.7.tar.gz --no-check-certificate -P /data &&
+  tar -zxf keepalived-2.2.7.tar.gz &&
+  mv /data/keepalived-2.2.7 /data/keepalived &&
+  rm -rf /data/keepalived-2.2.7.tar.gz &&
+  cd /data/keepalived
+
+# 编译安装
+./configure && make && make install
+
+# 复制文件到系统
+mkdir /etc/keepalived &&
+  cp /data/keepalived/keepalived/etc/init.d/keepalived /etc/init.d/ &&
+  cp /data/keepalived/keepalived/etc/keepalived/keepalived.conf.sample /etc/keepalived/keepalived.conf &&
+  cp /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.backup
+
+# 配置vim /etc/keepalived/keepalived.conf
+
+systemctl start keepalived && systemctl enable keepalived
